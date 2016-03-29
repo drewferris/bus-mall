d75fc6f7a846ae8a30e77aa039069523dc2dea9e
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-03-29 11:25:22 -0700

    can generate three random images

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function handleImageClick (event) {
   console.log('event.target: ', event.target);
   console.log('hello from handleImageClick event handler.');
@@ -9,38 +13,46 @@ for (var i = 0; i < imagesToBeClicked.length; i++){
   imagesToBeClicked[i].addEventListener('click', handleImageClick);
 }
 
-var createImage = function(src, title) {
-  var img = new Image();
-  img.src = src;
-  img.alt = title;
-  img.title = title;
-  return img;
-};
+function CreateImage(src, title){
+  this.src = src;
+  this.alt = title;
+  this.title = title;
+  this.numClicks = 0;
+  this.numDisplay = 0;
+}
 
 var images = [];
 
-images.push(createImage('img/bag.jpg', 'bag title'));
-images.push(createImage('img/banana.jpg', 'banana title'));
-images.push(createImage('img/bathroom.jpg', 'bathroom title'));
-images.push(createImage('img/boots.jpg', 'boots title'));
-images.push(createImage('img/breakfast.jpg', 'breakfast title'));
-images.push(createImage('img/bubblegum.jpg', 'bubblegum title'));
-images.push(createImage('img/chair.jpg', 'chair title'));
-images.push(createImage('img/cthulhu.jpg', 'cthulhu title'));
-images.push(createImage('img/dog-duck.jpg', 'dog-duck title'));
-images.push(createImage('img/dragon.jpg', 'dragon title'));
-images.push(createImage('img/pen.jpg', 'pen title'));
-images.push(createImage('img/pet-sweep.jpg', 'pet-sweep title'));
-images.push(createImage('img/scissors.jpg', 'scissors title'));
+images.push(new CreateImage('img/bag.jpg', 'bag title'));
+images.push(new CreateImage('img/banana.jpg', 'banana title'));
+images.push(new CreateImage('img/bathroom.jpg', 'bathroom title'));
+images.push(new CreateImage('img/boots.jpg', 'boots title'));
+images.push(new CreateImage('img/breakfast.jpg', 'breakfast title'));
+images.push(new CreateImage('img/bubblegum.jpg', 'bubblegum title'));
+images.push(new CreateImage('img/chair.jpg', 'chair title'));
+images.push(new CreateImage('img/cthulhu.jpg', 'cthulhu title'));
+images.push(new CreateImage('img/dog-duck.jpg', 'dog-duck title'));
+images.push(new CreateImage('img/dragon.jpg', 'dragon title'));
+images.push(new CreateImage('img/pen.jpg', 'pen title'));
+images.push(new CreateImage('img/pet-sweep.jpg', 'pet-sweep title'));
+images.push(new CreateImage('img/scissors.jpg', 'scissors title'));
 console.log(images);
-images.push(createImage('img/shark.jpg', 'shark title'));
-images.push(createImage('img/sweep.png', 'sweep title'));
-images.push(createImage('img/tauntaun.jpg', 'tauntaun title'));
-images.push(createImage('img/unicorn.jpg', 'unicorn title'));
-images.push(createImage('img/usb.gif', 'usb title'));
-images.push(createImage('img/water-can.jpg', 'water-can title'));
-images.push(createImage('img/wine-glass.jpg', 'wine-glass title'));
+images.push(new CreateImage('img/shark.jpg', 'shark title'));
+images.push(new CreateImage('img/sweep.png', 'sweep title'));
+images.push(new CreateImage('img/tauntaun.jpg', 'tauntaun title'));
+images.push(new CreateImage('img/unicorn.jpg', 'unicorn title'));
+images.push(new CreateImage('img/usb.gif', 'usb title'));
+images.push(new CreateImage('img/water-can.jpg', 'water-can title'));
+images.push(new CreateImage('img/wine-glass.jpg', 'wine-glass title'));
 
-function loadThreeRandomImages (images){
-  random
+function getThreeRandomImage(images){
+  var imageIndexOne = getRandomIntInclusive(0, images.length - 1);
+  var imageIndexTwo = getRandomIntInclusive(0, images.length - 1);
+  var imageIndexThree = getRandomIntInclusive(0, images.length - 1);
+
+  var imageOne = images[imageIndexOne];
+  var imageTwo = images[imageIndexTwo];
+  var imageThree = images[imageIndexThree];
+  return [imageOne, imageTwo, imageThree];
+
 };
