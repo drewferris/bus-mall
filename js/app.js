582ea7befb8acbb3c726fbf85d0556f20784c7b0
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-03-30 17:53:46 -0700

    still neeed to track images and display before moving to graph

--- js/app.js
+++ js/app.js
@@ -1,38 +1,24 @@
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 function handleImageClick (event) {
-  // console.log('event.target: ', event.target);
-  // console.log('hello from handleImageClick event handler.');
-  // displayThreeImages();
   globalTotalClicks++;
-  if(globalTotalClicks < 25) {
-    displayThreeImages();
-  } else {
+  if(globalTotalClicks === 25) {
     alert('you clicked 25 times');
+    return;
   }
-}
+  for(var i = 0; i < images.length; i++){
+    var currentImgId = event.target.id;
+    console.log('currentImageId:', currentImgId);
+    if(images[i].id === currentImgId){
+      images[i].numClicks++;
+    }
+  }
 
-var imagesToBeClicked = document.getElementsByClassName('images-to-be-clicked');
+  displayThreeImages();
 
-for (var i = 0; i < imagesToBeClicked.length; i++){
-  imagesToBeClicked[i].addEventListener('click', handleImageClick);
-}
-
-handleImageClick();
-
-// var firstImage = document.createElement('img');
-// firstImage.setAttribute('src', imageOne);
-// document.getElementById('image-one').appendChild(firstImage);
-
-// var secondImage = document.createElement('img');
-// secondImage.setAttribute('src', imageTwo);
-// document.getElementById('image-two').appendChild(secondImage);
-
-// var thirdImage = document.createElement('img');
-// thirdImage.setAttribute('src', imageThree);
-// document.getElementById('image-three').appendChild(thirdImage);
+};
 
 function CreateImage(src, title){
   this.src = src;
@@ -40,11 +26,10 @@ function CreateImage(src, title){
   this.title = title;
   this.numClicks = 0;
   this.numDisplay = 0;
-}
+};
 
 var images = [];
 var globalTotalClicks = 0;
-var globalImageDisplayedTotal = 0;
 
 images.push(new CreateImage('img/bag.jpg', 'bag title'));
 images.push(new CreateImage('img/banana.jpg', 'banana title'));
@@ -59,7 +44,7 @@ images.push(new CreateImage('img/dragon.jpg', 'dragon title'));
 images.push(new CreateImage('img/pen.jpg', 'pen title'));
 images.push(new CreateImage('img/pet-sweep.jpg', 'pet-sweep title'));
 images.push(new CreateImage('img/scissors.jpg', 'scissors title'));
-console.log(images, ' are the images');
+console.log(images + ' are the images');
 images.push(new CreateImage('img/shark.jpg', 'shark title'));
 images.push(new CreateImage('img/sweep.png', 'sweep title'));
 images.push(new CreateImage('img/tauntaun.jpg', 'tauntaun title'));
@@ -70,21 +55,22 @@ images.push(new CreateImage('img/wine-glass.jpg', 'wine-glass title'));
 
 function getThreeRandomImage(images){
   var imageIndexOne = getRandomIntInclusive(0, images.length - 1);
-  var imageIndexTwo = getRandomIntInclusive(0, images.length - 1);
-  var imageIndexThree = getRandomIntInclusive(0, images.length - 1);
+
+  var imageIndexTwo = imageIndexOne;
+  while (imageIndexTwo === imageIndexOne){
+    var imageIndexTwo = getRandomIntInclusive(0, images.length - 1);
+  }
+
+  var imageIndexThree = imageIndexOne;
+  while (imageIndexThree === imageIndexOne || imageIndexThree === imageIndexTwo){
+    var imageIndexThree = getRandomIntInclusive(0, images.length - 1);
+  }
 
   var imageObjOne = images[imageIndexOne];
   var imageObjTwo = images[imageIndexTwo];
   var imageObjThree = images[imageIndexThree];
   return [imageObjOne, imageObjTwo, imageObjThree];
-
 };
-
-// function deleteImages(imageOne, imageTwo, imageThree) {
-//   firstImage.removeChild(imageOne);
-//   secondImage.removeChild(imageTwo);
-//   thirdImage.removeChild(imgThree);
-// }
 
 function displayThreeImages() {
   var imageSurvayContainer = document.getElementById('image-survay-container');
@@ -92,32 +78,68 @@ function displayThreeImages() {
   var threeImageObjects = getThreeRandomImage(images);
   var imageOne = document.createElement('img');
   imageOne.setAttribute('src', threeImageObjects[0].src);
+  imageOne.setAttribute('id', threeImageObjects[0].title);
   var imageTwo = document.createElement('img');
   imageTwo.setAttribute('src', threeImageObjects[1].src);
+  imageTwo.setAttribute('id', threeImageObjects[1].title);
   var imageThree = document.createElement('img');
   imageThree.setAttribute('src', threeImageObjects[2].src);
+  imageThree.setAttribute('id', threeImageObjects[2].title);
+
+  imageOne.addEventListener('click', handleImageClick);
+  imageTwo.addEventListener('click', handleImageClick);
+  imageThree.addEventListener('click', handleImageClick);
+
   imageSurvayContainer.appendChild(imageOne);
   imageSurvayContainer.appendChild(imageTwo);
   imageSurvayContainer.appendChild(imageThree);
-}
+};
 
 displayThreeImages();
 
-// function userImageClickEvent(event) {
-//   globalTotalClicks++;
-//   if(globalTotalClicks < 25) {
-//     displayThreeImages();
-//   } else {
-//     alert('you clicked 25 times');
-//   }
-//   for(var i = 0; i < images.length; i++){
-//     var currentImgSrc = event.target.src;
-//     if(images[i].src === currentImgSrc)
-//       images[i].numClicks++;
-//   }
-// }
-// var imagesToBeClicked = document.getElementsByClassName('images-to-be-clicked');
+// //code review
+// var clicksforgraph = newBarDataSet('clicks', 'rgba('','',''));
+// clicksforgraph.getFields(images, 'numClicks');
 //
-// for (var i = 0; i < imagesToBeClicked.length; i++){
-//   imagesToBeClicked[i].addEventListener('click', handleImageClick);
+// var displaysforgraph = newBarDataSet('cdisplays', 'rgba('','',''));
+// clicksforgraph.getFields(images, 'numDisplay');
+//
+// var percent = newBarDataSet('cdisplays', 'rgba('','',''));
+// clicksforgraph.setFields(images, 'timesClicked', 'numDisplay');
+// function Bardataset() {
+//   this.label
+//   fillColor
+//   strokeColor
+//   highlightFill
+//   highlightStroke
+//   this.data = []
 // }
+// function BarChartData() {
+//
+// }
+//
+// Bardataset.prototype.getFields = function(imputArry, field)
+// for (var i = 0; i < images.length; i++) {}
+// this.data.push(images[i][field])
+//
+// Bardataset.prototype.getPercentclicked = function (images, field1, field2)
+// for( var i = 0; i < images.length ; i++; {
+//   var percentCLicked = parseInt(images[i][field1]) / parseInt(image[i][field2])
+//   if (isNaN(percentCLicked)) {
+//   this.data.push(0)
+// } else {
+//   this.data.push(percentCLicked);
+// }
+// }
+//
+// Bardataset.prototype.getLabels = function(imputArry, field)
+// for (var i = 0; i < images.length; i++) {}
+// this.data.push(images[i][field])
+//
+// var setupbarDataChart = new BarChartData();
+// setUpBarChart.pushData(clicksforgraph)
+// setUpBarChart.pushData(displaysforgraph)
+// setUpBarChart.pushData(percentCLicked)
+// setUpBarChart.getLabel(cimages, 'title');
+//
+// var ctx = document.getElementById('myChart').getContext('2d')
